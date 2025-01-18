b9c0851876398cdc54cbe78d8a5b0921289d5adb
alneoru <alneo.ru@yandex.ru>
2025-01-18 15:58:32 +0300

    Константы проекта, проверка версии PHP

---
+++ config/init.php
@@ -0,0 +1,8 @@
+<?php
+if (PHP_MAJOR_VERSION<8) die('Requires PHP version >= 8');
+define('ROOT',dirname(__DIR__));
+const WWW = ROOT .'/public';
+const APP = ROOT .'/app';
+const CORE = ROOT .'/core';
+const HELPERS = ROOT .'/helpers';
+const CONFIG = ROOT .'/config';
